91cf20b8e5f0ea011bedca3d56715ed71b1a60ab
Precious <banigoprecious@yahoo.com>
2025-03-04 14:33:07 +0100

    pollify.up.railway.app

--- poll_system/poll_system/settings.py
+++ poll_system/poll_system/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pollify.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pollify.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['https://pollify.com']
+CSRF_TRUSTED_ORIGINS = ['https://pollify.up.railway.app']
 
 
 # Application definition
