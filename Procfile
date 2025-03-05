904bf0bd3cff8a1519f711b6a2fb95ffc5c13793
Precious <banigoprecious@yahoo.com>
2025-03-05 08:43:45 +0100

    ...

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn poll_system.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn poll_system.wsgi --log-file -
--- poll_system/poll_system/settings.py
+++ poll_system/poll_system/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pollify.up.railway.app', 'https://pollify.up.railway.app']
+ALLOWED_HOSTS = ['pollify.up.railway.app', 'https://pollify.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://pollify.up.railway.app']
 
